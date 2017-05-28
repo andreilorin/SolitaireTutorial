75e0ae01481a4a8e2cf023a24de6f74f70ca5980
andrei lorin <andreilorin@gmail.com>
2017-05-28 20:28:58 +0100

    created Presenter and View

---
+++ src/Presenter.kt
@@ -0,0 +1,27 @@
+object Presenter {
+    var view: View? = null
+
+    fun setGameView(gameView: View) {
+        view = gameView
+    }
+
+    fun onDeckTap() {
+        GameModel.onDeckTap()
+        view?.update()
+    }
+
+    fun onWasteTap() {
+        GameModel.onWasteTap()
+        view?.update()
+    }
+
+    fun onFoundationTap(foundationIndex: Int) {
+        GameModel.onFoundationTap(foundationIndex)
+        view?.update()
+    }
+
+    fun onTableauTap(tableauIndex: Int, cardIndex: Int) {
+        GameModel.onTableauTap(tableauIndex, cardIndex)
+        view?.update()
+    }
+}
---
+++ src/View.kt
@@ -0,0 +1,3 @@
+interface View {
+    fun update(model: GameModel = GameModel)
+}
